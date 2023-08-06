8465fe6b3fcd207588be56d661c21de0df044b3a
BB1464 <oluwafemioyedele908@gmail.com>
2023-08-06 17:53:25 +0100

    Add excercise

---
+++ excercises/data-import-practical.R
@@ -0,0 +1,6 @@
+## Data Import Practicals
+
+# Import `sorghum.xlsx` into RStudio
+# and assigned it to an object called `sorghnum_dat.`
+
+# View the first 6 rows of the dataset.
---
+++ excercises/data-wrangling-practical.R
@@ -0,0 +1,27 @@
+## Data Wrangling: Practical
+
+# Import `sorghum.xlsx` to R and
+# save to an object named example03
+
+
+
+# How many observations do we have per environment?
+
+
+
+# How many `environment?`
+#`genotypes?` Display the list of
+#locations and the list of `genotypes respectively?`
+
+
+# Display the data where
+#the `yield was less than 150?`
+
+
+
+# Select `env`, `gen`, `yield`, to be saved in a
+#new object `example03.short` and
+#create a new variable `mean_yield`
+#by by computing the mean of the `yield.`
+
+
